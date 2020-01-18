ab6e289204c0f4a966c2e2b793482adc74c79c12
Team <teammiric@gmail.com>
2020-01-18 18:43:36 +0000

    added app.run() method

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+import git
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -24,13 +25,29 @@ class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+
+    origin.pull()
+    return 'Updated PythonAnywhere successfully', 200
+#    else:
+#        return 'Wrong event type', 400
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", food=food.query.all())
+        return render_template("main_page.html", food=Food.query.all())
 
     food = Food(content=request.form["contents"])
     db.session.add(food)
     db.session.commit()
     return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run()
+
